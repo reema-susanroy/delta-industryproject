03449d3df1e2ab7e41cab9f48f91f5b13021ba36
tatianaTun <tatianatun92@gmail.com>
2024-03-21 22:00:50 -0700

    Initialized the project folder

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+
+        {/* <script
+          async
+          src="https://cse.google.com/cse.js?cx=847e676e3f84f4d97"
+        ></script>
+        <div className="gcse-search"></div> */}
+
+    </div>
+  );
+}
+
+export default App;
