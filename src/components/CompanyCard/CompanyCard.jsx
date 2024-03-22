bed87924f717fafeef0605ede33e9d42db9f27df
Reema Roy <reemaroy73@gmail.com>
2024-03-22 09:15:10 -0700

    Changed gap

--- src/components/CompanyCard/CompanyCard.jsx
+++ src/components/CompanyCard/CompanyCard.jsx
@@ -4,22 +4,22 @@ function CompanyCard( {}) {
     return (
         <>
             <div className='companyCard__header'>
-                <h3 className='companyCard__header--name'> Company Name </h3>
+                <h3 className='companyCard__header--name'> Company </h3>
                 <h3 className='companyCard__header--details'> Details </h3>
             </div>
 
 
             <div className="companyCard__content">
-                <div>
+                <div className='border'> 
                     <p className='companyCard__content--name' >Blog NAme</p>
                 </div>
-                <div>
+                <div className='border'>
 
                     <p className='companyCard__content--url'> url</p>
                     <p className='companyCard__content--author'> authir name</p>
                     <p className='companyCard__content--contact'>conatt info</p>
                 </div>
-                <div>
+                <div className='border'>
                     <button className='companyCard__button--button'> Connect </button>
                 </div>
             </div>
